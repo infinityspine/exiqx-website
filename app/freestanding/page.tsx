10852262f571b0b3f66fd1dc8799c0e6ea01834a
Infinity Spine Center <infinityspinecenter@Infinitys-MacBook-Pro.local>
2025-11-09 14:27:48 -0700

    🕒 Auto-commit: 2025-11-09 14:27:48

---
+++ app/freestanding/page.tsx
@@ -0,0 +1,7 @@
+export default function FreestandingPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1>Freestanding Footplate Page</h1>
+    </main>
+  )
+}
