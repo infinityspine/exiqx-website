ca8be8274a57f483004f0e463668ac9a90bcbedd
Infinity Spine Center <infinityspinecenter@Infinitys-MacBook-Pro.local>
2025-11-10 17:46:28 -0700

    🕒 Auto-commit: 2025-11-10 17:46:28

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import NavBar from '@/components/layout/NavBar'
 import HeroSection from '@/components/sections/HeroSection'
 import FootplateFeatures from '@/components/sections/FootplateFeatures'
 import UseCases from '@/components/sections/UseCases'
 import Testimonials from '@/components/sections/Testimonials'
 import HomePageCTA from '@/components/sections/HomePageCTA'
-import Footer from '@/components/layout/Footer'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white overflow-x-hidden">
-      <NavBar />
       <HeroSection />
       <FootplateFeatures />
       <UseCases />
       <Testimonials />
       <HomePageCTA />
-      <Footer />
     </main>
   )
 }
