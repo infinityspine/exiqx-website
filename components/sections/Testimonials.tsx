241f8fe6033ccccfb89ecf1d78cc29e32790889d
Infinity Spine Center <infinityspinecenter@Infinitys-MacBook-Pro.local>
2025-11-12 19:29:30 -0700

    🕒 Auto-commit: 2025-11-12 19:29:30

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -87,7 +87,7 @@ const Testimonials = memo(function Testimonials({
                 <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
               </svg>
               <div className="absolute inset-0 rounded-2xl bg-red-500/10 blur-xl" aria-hidden="true" />
-            </motion.div>
+            </div>
 
             {/* Quote */}
             <blockquote className="relative">
@@ -141,7 +141,7 @@ const Testimonials = memo(function Testimonials({
                 <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
               </svg>
               <div className="absolute inset-0 rounded-2xl bg-red-500/10 blur-xl" aria-hidden="true" />
-            </motion.div>
+            </div>
 
             {/* Quote */}
             <blockquote className="relative">
@@ -195,7 +195,7 @@ const Testimonials = memo(function Testimonials({
                 <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
               </svg>
               <div className="absolute inset-0 rounded-2xl bg-red-500/10 blur-xl" aria-hidden="true" />
-            </motion.div>
+            </div>
 
             {/* Quote */}
             <blockquote className="relative">
